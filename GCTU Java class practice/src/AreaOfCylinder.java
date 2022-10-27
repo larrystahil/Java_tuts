c19c2e0299d6ff7a94fe3b7be3e86d190618e27c
larrystahil <larrystahil2468@gmail.com>
2022-10-27 08:43:21 +0000

    Hello Java

--- GCTU Java class practice/src/AreaOfCylinder.java
+++ GCTU Java class practice/src/AreaOfCylinder.java
@@ -9,9 +9,9 @@ public class AreaOfCylinder {
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.print("****************************************\nArea and Volume Of A Cylinder Application\n***************************************\n\nKindly Enter The Radius Of Your Cylinder: ");
+        System.out.print("*******************************************\n Area and Volume Of A Cylinder Application\n*******************************************\n\nKindly Enter The Radius Of Your Cylinder: ");
         double useRadius = input.nextDouble();
-        System.out.println("Enter Your length To Calculate The Volume in centimeter(cm): ");
+        System.out.print("Enter Your length To Calculate The Volume in centimeter(cm): ");
         double userLenghth = input.nextDouble();
         double result1 = area(useRadius);
         double volume = result1 * userLenghth;
---
+++ GCTU Java class practice/src/Discount_app.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Discount_app {
+    float originalPrice, sellingPrice;
+
+    //public static float discount(){
+      //  float disc = originalPrice - sellingPrice;
+        //return disc;
+    //}
+
+    public static void main(String[] args){
+        System.out.println("***************************\nA Discount Calculating Program\n***************************");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Original of the your product: ");
+
+    }
+}
