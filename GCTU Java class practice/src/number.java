f76d6579a9bf02dbb42736fd40547dc40604a4a7
larrystahil <larrystahil2468@gmail.com>
2022-11-01 12:37:54 +0000

    Hello Java

---
+++ GCTU Java class practice/src/number.java
@@ -0,0 +1,23 @@
+/*
+import javax.swing.plaf.synth.SynthTextAreaUI;
+import java.util.Scanner;
+
+public class number {
+    public static void main(String[] args){
+        Scanner num = new Scanner(System.in);
+        System.out.print("Enter the Name of your School: ");
+        String name = num.nextLine();
+        System.out.print("Enter the program you are offering: ");
+        String program = num.nextLine();
+        System.out.println("Your school name is: "+name+" and you offer "+program);
+    }
+}
+*/
+public class number {
+    int x = 5;
+
+    public static void main(String[] args) {
+        number myObj = new number();
+        System.out.println(myObj.x);
+    }
+}
--- GCTU Java class practice/src/paymentForLecturers.java
+++ GCTU Java class practice/src/paymentForLecturers.java
@@ -1,14 +1,10 @@
 import java.util.Scanner;
 public class paymentForLecturers {
-
-
     final static int bonus = 500;
     final static double hrRate= 100;
     final static double welfarePerMnth = 70;
-
     public static void main(String[]args){
         double  grossPay, hrsPerMnth, salary, bonusPay, VAT;
-
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the Number of Hours you worked in a Month: ");
         hrsPerMnth = input.nextInt();
@@ -17,10 +13,7 @@ public class paymentForLecturers {
         VAT= (17.5/100) * grossPay;
         double deduction = VAT + welfarePerMnth;
         salary = bonusPay - deduction;
-
         //System.out.println(grossPay + " salary is "+ salary);
-
-
         if (hrsPerMnth > 20){
             bonusPay = grossPay + bonus;
             salary = bonusPay - deduction;
@@ -30,9 +23,5 @@ public class paymentForLecturers {
             salary = grossPay - deduction;
             System.out.println("Your Hours worked in the month is "+ hrsPerMnth+" hours Salary is GHC "+ salary);
         }
-
-
-
-
     }
 }
